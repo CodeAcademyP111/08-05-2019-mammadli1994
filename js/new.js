807cd6c2a4a90e0fe00765173d2cd03b40cccdd9
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-05-08 12:50:08 +0400

    Add files via upload

---
+++ js/new.js
@@ -0,0 +1,48 @@
+// var str="paytaxt|test";
+
+// var arr = [14, 34, 100, -1, 15, 25];
+// arr.pop();
+// arr.push(15);
+// arr.shift();
+// arr.unshift(69);
+// arr.pop();
+// arr[arr.length] = 50;
+// delete arr[2];
+
+// var partOfArray=arr.splice(1);
+// var partOfArray=arr.slice(1);
+// console.log(partOfArray);
+// console.log(arr.sort(function (a, b) { return a - b }));
+
+// function arrMin(arr) {
+//     var min = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (min > arr[i]) {
+//             min = arr[i];
+//         }
+//     }
+//     return min;
+// }
+
+// function arrMax(arr) {
+//     var max = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (max < arr[i]) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(arrMax(arr));
+var date = new Date();
+var day=new Date(1990,3,15);
+
+day.setFullYear(1991);
+day.setDate(14)
+
+// console.log(date);
+console.log(day);
+
+// var diff=date.getTime()-birthday.getTime();
+// console.log(diff);
---
+++ js/script.js
@@ -0,0 +1,56 @@
+//Ev tapshiriqlari
+
+// var n = 52;
+
+// function getNumberFloor(n) {
+//     var count = 0;
+//     while (n > 0) {
+//         n = (n - n % 10) / 10;
+//         count++;
+//     }
+//     return count;
+// }
+// console.log(getNumberFloor(n)+" "+n);
+// document.write(getNumberFloor(n));
+
+// var word = "Salam";
+// var n = 3;
+
+// function contactWord(soz, say) {
+//     var result = "";
+//     for (var i = 0; i < say; i++) {
+//         result += soz;
+//     }
+//     return result;
+// }
+
+// console.log(contactWord("Sagol", 4));
+
+
+//Praktika
+
+// function ul(n) {
+//     document.write("<ul>");
+
+//     for (var i = 0; i < n; i++) {
+//         document.write("<li>Salam</li>");
+//     }
+
+//     document.write("</ul>");
+// }
+
+// ul(5)
+
+
+// function creatElm(count, colm) {
+//     document.write('<div class="main">');
+//     var width=(100-colm)/colm;
+
+//     for (var i = 0; i < count; i++) {
+//         document.write(`<div class="item" style="width:${width}%"></div>`);
+//     }
+
+//     document.write('</div>');
+// }
+
+// creatElm(10,5);
