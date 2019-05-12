dda839c75a28b4b1ac22d16b10536d55aa24abb3
mammadli1994 <49428069+mammadli1994@users.noreply.github.com>
2019-05-12 19:05:48 +0400

    Add files via upload

---
+++ 08.05.19/js/script.js
@@ -0,0 +1,31 @@
+// var arr=[12,21,54,5,54,12,21];
+// var a=[]
+
+// function findDublicate(a){
+// console.log(arr.sort(function(a,b){return a-b}));
+// for( var i=0; i<arr.length; i++ ) {
+//     if ( arr[i] == arr[i+1] ) {
+        
+//         a.push(arr[i]);
+//     }
+    
+// }   
+// return a;
+
+// }
+// console.log(findDublicate(a))
+
+var date=new Date();
+
+function instaDate(n){
+
+    for(var i=0; i<=n; i++){
+        if(date.getHours(23+n)){
+
+        }
+    }
+    return Date;
+}
+
+
+console.log(instaDate(5));
